7070861e7e63a11d3638b888af3654ffc2c4f23a
Satyaa Goyal <49252920+CrimsonDevil333333@users.noreply.github.com>
2019-09-14 12:33:35 +0530

    Update reciver.py

--- database/reciver.py
+++ database/reciver.py
@@ -10,7 +10,7 @@ def Mail_Recever(m):
     try:
         s = smtplib.SMTP('smtp.gmail.com', 587) 
         s.starttls() 
-        s.login("crim333333@gmail.com", "shashikant@1")
+        s.login("crim333333@gmail.com", "PASS")
         s.sendmail("crim333333@gmail.com", "crim333333@gmail.com", m) 
         s.quit()
     except:
